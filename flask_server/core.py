878d8b1e652db5ab895afed9bc9f0d3a28418541
yanbofei <yanbofei@papayamobile.com>
2017-06-20 15:58:07 +0800

    xxx

---
+++ flask_server/core.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+def rgb2gray():
+    import pdb; pdb.set_trace()
+    img_fd = Image.open('xxx')
+    width, height = img_fd.size
+    import pdb; pdb.set_trace()
+    print('xxx')
+
+
+
+if __name__ == '__main__':
+    rgb2gray()
